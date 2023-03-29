78e8d0a263592daee82c5d85de5603da82346030
yellowsy <hsyhuang1028@foxmail.com>
2023-03-29 16:49:46 +0800

    第一次上传

---
+++ cloud-config-client-3355/src/main/java/com/hsy/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,17 @@
+package com.hsy.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope //刷新功能，同步github上的配置文件内容
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
